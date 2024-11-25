cfcb4d46ec12e4250c0248467130a55906d8d013
root <djelloulrouba@yahoo.com>
2024-11-25 10:51:11 +0000

    Create script file and header file

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
-#include
+#include <stdarg.h>
+
+/**
+ * _printf - function that produces output according to a format
+ * @format: is a character string. The format string is composed
+ *          of zero or more directives
+ *
+ * Return: Always 0
+ */
+
+
+
+int _printf(const char *format, ...)
+{
+  format formats[] = {
+    {
+  
+}
