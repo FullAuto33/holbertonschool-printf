d32f42cd52eb3822fa4717581cd7f226c41c7137
root <djelloulrouba@yahoo.com>
2024-11-25 14:24:27 +0000

    Write function _printf with specified % and all charcatere

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
 
 /**
  * _printf - function that produces output according to a format
@@ -20,7 +22,38 @@ type_specifier_t type_specifier[] = {
 {'c', _print_char},
 {'s', _print_string},
 {'i', _print_int},
-{'d', _print_decimal},
+{'d', _print_int},
 {'\0', NULL}
 };
+
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+char specifier = *format;
+int compteur  = 0;
+int truespecifier = 0;
+
+for (; type_specifier[compteur].specifier != '\0'; compteur++)
+{
+if (type_specifier[compteur].specifier == specifier)
+{
+type_specifier[compteur].print_func(args);
+truespecifier = 1;
+}
+}
+if (truespecifier != 1)
+{
+_putchar('%');
+_putchar(specifier);
+}
+}
+else
+{
+putchar(*format);
+}
+format++;
+}
+va_end(args);
 }
